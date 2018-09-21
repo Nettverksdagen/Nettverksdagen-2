0fcefece78a8e39a99194867acd0c0831a750916
sigtot <sigurdtotland@gmail.com>
2018-09-21 22:40:55 +0200

    Add Vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
