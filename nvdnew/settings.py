fcd3b5e399a3720f998678befccb1b98f547b53e
sigtot <sigurdtotland@gmail.com>
2018-10-08 01:38:16 +0200

    Set permissions for REST framework

--- nvdnew/settings.py
+++ nvdnew/settings.py
@@ -30,6 +30,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
