f8fc2dbe5594613abe88184cd9401acf4cad446b
sigtot <sigurdtotland@gmail.com>
2018-11-23 14:40:59 +0100

    Improve docker setup

--- api/django-entrypoint.sh
+++ api/django-entrypoint.sh
@@ -5,7 +5,10 @@ while ! nc -z postgres 5432; do
   sleep 0.1
 done
 
-echo "Applying database migrations"
+echo "Entering api dir"
+cd api
+
+echo "Applying all database migrations"
 python3 manage.py migrate
 
 echo "Starting server"
