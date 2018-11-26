370776ce1f6a3f232431a42f271c1f998b6a3726
sigtot <sigurdtotland@gmail.com>
2018-11-27 00:58:26 +0100

    Remove unwanted methods from CORS headers

--- fileserver/main.go
+++ fileserver/main.go
@@ -104,8 +104,8 @@ func httpError(w http.ResponseWriter, errorCode int) {
 }
 
 func addCORSHeader(w http.ResponseWriter)  {
-    (w).Header().Set("Access-Control-Allow-Origin", "*")
-    (w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    (w).Header().Set("Access-Control-Allow-Headers",
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers",
         "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
