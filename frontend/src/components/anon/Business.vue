4b2c8c7913cccff9aa177e13e66f35c8611ad65b
sigtot <sigurdtotland@gmail.com>
2018-12-03 16:45:01 +0100

    Make padding on company logos responsive

--- frontend/src/components/anon/Business.vue
+++ frontend/src/components/anon/Business.vue
@@ -16,7 +16,29 @@ export default {
 <style lang="scss" scoped>
 .business {
   transition:0.2s;
-  padding: 5rem;
+  padding: 1rem;
+  @media(min-width: 576px) {
+    // No hover effect on tiny devices
+    padding: 2rem;
+  }
+  @media(min-width: 768px) {
+    padding: 3rem;
+    &:hover {
+      padding: 2rem;
+    }
+  }
+  @media(min-width: 992px) {
+    padding: 4rem;
+    &:hover {
+      padding: 3rem;
+    }
+  }
+  @media(min-width: 1200px) {
+    padding: 5rem;
+    &:hover {
+      padding: 4rem;
+    }
+  }
   &:hover {
     padding:4rem;
   }
