edd56a438cd1a33a037ef1749de73482398db6ac
sigtot <sigurdtotland@gmail.com>
2019-01-12 00:08:35 +0100

    Implement thumbnail creation in fileserver

--- fileserver/main.go
+++ fileserver/main.go
@@ -2,25 +2,34 @@ package main
 
 import (
     "bytes"
+    "errors"
+    "fmt"
     "github.com/google/uuid"
+    "github.com/nfnt/resize"
     "image"
+    "image/draw"
+    "image/jpeg"
     _ "image/jpeg"
+    "image/png"
     _ "image/png"
     "io/ioutil"
     "log"
     "mime"
     "net/http"
+    "os"
+    "strconv"
 )
 
 const uploadDir = "./uploads"
+const thumbNailDir = uploadDir + "/thumb"
 
 var legalContentTypes = map[string]bool {
     "image/png": true,
     "image/jpeg": true,
     "image/svg+xml": true,
 }
-
 func main() {
+    fmt.Println("Starting fileserver...")
     http.HandleFunc("/upload/image", uploadHandler)
     fs := http.FileServer(http.Dir(uploadDir))
     http.Handle("/", fs)
@@ -61,7 +70,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    _, _, err = image.Decode(bytes.NewReader(bs))
+    img, _, err := image.Decode(bytes.NewReader(bs))
     if err != nil {
         log.Println(err)
         httpError(w, http.StatusInternalServerError)
@@ -90,6 +99,16 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    thumbImg := createThumbNail(img)
+    saveThumb(thumbImg, -1, fileUUID.String(), fileExts[0])
+    saveThumb(thumbImg, 256, fileUUID.String(), fileExts[0])
+    saveThumb(thumbImg, 512, fileUUID.String(), fileExts[0])
+    if err != nil {
+        log.Println(err)
+        httpError(w, http.StatusInternalServerError)
+        return
+    }
+
     w.WriteHeader(http.StatusOK)
     _, err = w.Write([]byte(fileURI))
     if err != nil {
@@ -97,6 +116,67 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
         httpError(w, http.StatusInternalServerError)
         return
     }
+}
+
+// Center images in their square bound
+func createThumbNail(img image.Image) image.Image {
+    imgWidth := img.Bounds().Max.X
+    imgHeight := img.Bounds().Max.Y
+
+    horizontal := imgWidth >= imgHeight
+    thumbNailSize := max(imgWidth, imgHeight)
+
+    var offsetX, offsetY int
+    if horizontal {
+        offsetX = 0
+        offsetY = (imgWidth - imgHeight) / 2
+    } else {
+        offsetX = (imgHeight - imgWidth) / 2
+        offsetY = 0
+    }
+
+    thumbImg := image.NewRGBA(image.Rectangle{Min: image.Point{X: 0, Y: 0}, Max: image.Point{X: thumbNailSize, Y: thumbNailSize}})
+    draw.Draw(
+        thumbImg,
+        image.Rectangle{
+            Min: image.Point{X: img.Bounds().Min.X + offsetX, Y: img.Bounds().Min.Y + offsetY},
+            Max: image.Point{X: img.Bounds().Max.X + offsetX, Y: img.Bounds().Max.Y + offsetY}},
+        img,
+        image.Point{X: 0, Y: 0},
+        draw.Src)
+
+    return thumbImg
+}
+
+// Save the thumbnail image
+// Scale it to match size and save in /thumb/[size]/ if size is anything but -1
+func saveThumb(img image.Image, size int, fileName, fileExt string) error {
+    sizeDir := ""
+    resizedImg := img
+    if size != -1 {
+        sizeDir = "/" + strconv.Itoa(size)
+        resizedImg = resize.Resize(uint(size), 0, img, resize.Lanczos3)
+    }
+
+    err := os.MkdirAll(thumbNailDir + sizeDir, 0666)
+    if err != nil {
+        return err
+    }
+
+    file, err := os.Create(thumbNailDir + sizeDir + "/" + fileName + fileExt)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    if fileExt == ".png" {
+        err = png.Encode(file, resizedImg)
+    } else if fileExt == ".jpg" || fileExt == ".jpeg" {
+        err = jpeg.Encode(file, resizedImg, nil)
+    } else {
+        err = errors.New("bad file extension")
+    }
+    return err
 }
 
 func httpError(w http.ResponseWriter, errorCode int) {
@@ -108,4 +188,11 @@ func addCORSHeader(w http.ResponseWriter)  {
     w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
     w.Header().Set("Access-Control-Allow-Headers",
         "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
