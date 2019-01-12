62442ce41552b5aeddbfa26fc7cbfc594386ed59
Martin Brandt <martin_brandt97@hotmail.com>
2019-01-12 21:19:52 +0100

    Fixed bug with hr disappearing in hero

--- frontend/src/components/anon/HeroText.vue
+++ frontend/src/components/anon/HeroText.vue
@@ -14,10 +14,14 @@
 
   .hr-hero {
     border-top: solid 1px white;
+    height: 1px;
     width: 60%;
 
+    @media(min-width: 768px) {
+      width: 30%;
+    }
     @media (min-width: 992px) {
-      width: 40%;
+      width: 45%;
     }
   }
 
