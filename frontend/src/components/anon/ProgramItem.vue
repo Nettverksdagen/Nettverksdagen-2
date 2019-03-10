344a9a30cc5b5d4b729d79c585527a5287d55124
Martin Brandt <martin_brandt97@hotmail.com>
2019-03-10 11:20:29 +0100

    Added programItem template

---
+++ frontend/src/components/anon/ProgramItem.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="timeline-item">
+    <div class="timestamp">
+      <h4><span class="font-weight-bold">{{timeStart}}</span></h4>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <slot></slot>
+        <div v-if="place" class="d-inline">
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>
+          {{place}}
+        </div>
+        <div v-if="timeEnd" class="d-inline">
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>
+          {{timeStart}} - {{timeEnd}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarkerAlt, faClock } from '@fortawesome/free-solid-svg-icons'
+library.add(faMapMarkerAlt, faClock);
+export default {
+    name: "ProgramItem",
+    props: ['timeStart', 'timeEnd', 'place'],
+}
+</script>
+
+<style scoped lang="scss">
+  .card {
+    position: relative;
+    border:none;
+    border-radius: 0.5em;
+    background-color: white;
+    box-shadow: 0 1px 2px rgba(0,0,0,0.2), 0 1px 2px rgba(0,0,0,0.15);
+  }
+
+  .timeline-item {
+    padding: 15px 0 15px 40px;
+    position: relative;
+    background-color: inherit;
+  }
+
+  /* Add arrows to the right container (pointing left) */
+  .timeline-item::before {
+    content: " ";
+    height: 0;
+    position: absolute;
+    top: 22px;
+    width: 0;
+    z-index: 1;
+    left: 30px;
+    border: medium solid white;
+    border-width: 10px 10px 10px 0;
+    border-color: transparent white transparent transparent;
+  }
+
+  /* The circles on the timeline */
+  .timeline-item::after {
+    content: '';
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    right: -17px;
+    background-color: white;
+    border: 4px solid #1d4844;
+    top: 18px;
+    border-radius: 50%;
+    z-index: 1;
+    left: -13px;
+  }
+
+  .timestamp {
+    position: absolute;
+    left: -92px;
+    top: 16px;
+  }
+
+  @media(max-width: 768px) {
+    .timestamp {
+      top: 30px;
+      h4 {
+        font-size: 1.2em;
+      }
+    }
+
+    .timeline-item::after {
+      top: 30px;
+      left: -30px;
+    }
+
+    .timeline-item {
+      padding-left: 16px;
+    }
+
+    .timeline-item::before {
+      left: 8px;
+      top: 32px;
+    }
+  }
+</style>
--- frontend/src/views/anon/ProgramView.vue
+++ frontend/src/views/anon/ProgramView.vue
@@ -7,147 +7,20 @@
         <h3 class="font-weight-bold">30. januar</h3>
         <div class="timeline">
           <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">13.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Cisco</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL3
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>17.00 - 18.00</p>
-              </div>
-            </div>
+            <ProgramItem time-start="17:00" time-end="18:00" place="EL3">
+              <h4 class="mb-1">Bedriftspresentasjon med Cisco</h4>
+            </ProgramItem>
           </div>
         </div>
         <h3 class="font-weight-bold">31. januar</h3>
         <div class="timeline">
-          <div class="timeline-item">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">10.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h3 class="font-weight-bold">Nettverksdagen åpner</h3>
-                <p class="description">Alle bedriftene står klare i Glassgården kl 10 for å slå av en prat.</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">10.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med SINTEF</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>H1
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>10.00 - 11.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">11.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Disruptive Technologies</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>H1
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>11.00 - 12.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">12.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Data Respons</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL3
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>12.15 - 13.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">12.05</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h3 class="font-weight-bold">Ballongslipp</h3>
-                <p class="description">Møt opp tidlig for å sikre deg noen gode premier. Premiene deles ut kl 14.05.</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">13.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Mnemonic</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL4
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>13.00 - 14.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">13.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Sopra Steria</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL3
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>13.00 - 14.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">14.05</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h3 class="font-weight-bold m-0">Premier fra ballongslipp deles ut</h3>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">15.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h3 class="font-weight-bold m-0">Nettverksdagen er over</h3>
-                <p class="description">Standene i Glassgården pakker sammen, men det er enda et par bedriftpresentasjoner å få med seg!</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">15.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Honeywell</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL6
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>15.00 - 16.00</p>
-              </div>
-            </div>
-          </div>
-          <div class="timeline-item timeline-item-company">
-            <div class="timestamp">
-              <h4><span class="font-weight-bold">16.00</span></h4>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h4 class="mb-1">Bedriftspresentasjon med Imatis</h4>
-                <p class="description m-0"><font-awesome-icon :icon="{ prefix: 'fas', iconName: 'map-marker-alt' }" class="mr-1"/>EL3
-                  <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'clock' }" class="mr-1 ml-2"/>16.00 - 17.00</p>
-              </div>
-            </div>
-          </div>
+          <ProgramItem time-start="10:00">
+            <h3 class="font-weight-bold">Nettverksdagen åpner</h3>
+            <p class="description">Alle bedriftene står klare i Glassgården kl 10 for å slå av en prat.</p>
+          </ProgramItem>
+          <ProgramItem time-start="10:00" time-end="11:00" place="H1">
+            <h4 class="mb-1">Bedriftspresentasjon med SINTEF</h4>
+          </ProgramItem>
         </div>
       </div>
       <h2 id="stand-map-header" class="text-center mb-3">Standkart</h2>
@@ -157,13 +30,12 @@
 
 <script>
 import Content from '@/components/common/Content.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMapMarkerAlt, faClock } from '@fortawesome/free-solid-svg-icons'
-library.add(faMapMarkerAlt, faClock)
+import ProgramItem from '@/components/anon/ProgramItem.vue'
 export default {
   name: 'ProgramView',
   components: {
-    Content
+    Content,
+    ProgramItem
   }
 }
 </script>
@@ -205,59 +77,6 @@ export default {
     bottom: 0;
     margin-left: -3px;
   }
-  .timeline-item {
-    padding: 15px 0 15px 40px;
-    position: relative;
-    background-color: inherit;
-  }
-
-  /* Add arrows to the right container (pointing left) */
-  .timeline-item::before {
-    content: " ";
-    height: 0;
-    position: absolute;
-    top: 22px;
-    width: 0;
-    z-index: 1;
-    left: 30px;
-    border: medium solid white;
-    border-width: 10px 10px 10px 0;
-    border-color: transparent white transparent transparent;
-  }
-
-  /* The circles on the timeline */
-  .timeline-item::after {
-    content: '';
-    position: absolute;
-    width: 25px;
-    height: 25px;
-    right: -17px;
-    background-color: white;
-    border: 4px solid #1d4844;
-    top: 18px;
-    border-radius: 50%;
-    z-index: 1;
-    left: -13px;
-  }
-
-  .timeline-item-company::after {
-    //background-color: #45b7ae;
-  }
-
-  .timestamp {
-    position: absolute;
-    left: -92px;
-    top: 16px;
-  }
-
-  /* The actual content */
-  .card {
-    position: relative;
-    border:none;
-    border-radius: 0.5em;
-    background-color: white;
-    box-shadow: 0 1px 2px rgba(0,0,0,0.2), 0 1px 2px rgba(0,0,0,0.15);
-  }
 
   .btn-primary {
     background-color: #1d4844;
@@ -284,29 +103,8 @@ export default {
   }
 
   @media(max-width: 768px) {
-    .timestamp {
-      top: 30px;
-      h4 {
-        font-size: 1.2em;
-      }
-    }
-
     .timeline::after {
       margin-left: -20px;
-    }
-
-    .timeline-item::after {
-      top: 30px;
-      left: -30px;
-    }
-
-    .timeline-item {
-      padding-left: 16px;
-    }
-
-    .timeline-item::before {
-      left: 8px;
-      top: 32px;
     }
   }
 
