661c3b322d4b8325b003e8faf5242f0ec82b756b
sigtot <sigurdtotland@gmail.com>
2019-03-30 17:51:41 +0100

    Fix deploy

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ./backup.sh
+git reset --hard origin/master
+git pull
+
+yes | docker-compose -f docker-compose-prod.yml rm frontend
 docker-compose -f docker-compose-prod.yml build
 
 docker-compose -f docker-compose-prod.yml stop nginx
 yes | docker-compose -f docker-compose-prod.yml rm nginx
 docker volume rm nettverksdagen-2_frontend-files
-docker-compose -f docker-compose-prod.yml up --no-deps -d 
-yes | docker-compose -f docker-compose-prod.yml rm frontend
+docker-compose -f docker-compose-prod.yml up --no-deps -d --no-recreate
