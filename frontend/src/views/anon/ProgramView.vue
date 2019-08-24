5dba192cc2567d6183e001011e7aa2540b66d2e2
Erlend Blomseth <erlend@elendig.no>
2019-08-24 11:36:45 +0200

    Remove 2019 items from program

--- frontend/src/views/anon/ProgramView.vue
+++ frontend/src/views/anon/ProgramView.vue
@@ -4,22 +4,10 @@
         <h1 class="text-center">Program</h1>
         <p class="text-center description mt-3 mb-2">Dette er det foreløpige programmet for Nettverksdagene 2020.</p>
         <h4 class="text-center font-weight-bold"><a href="#stand-map-header">Se standkart her!</a></h4>
-        <h3 class="font-weight-bold">30. januar</h3>
+        <h3 class="font-weight-bold">29. januar</h3>
         <div class="timeline">
-          <div class="timeline-item timeline-item-company">
-            <ProgramItem time-start="17:00" time-end="18:00" place="EL3">
-              <h4 class="mb-1">Bedriftspresentasjon med Cisco</h4>
-            </ProgramItem>
-          </div>
-        </div>
-        <h3 class="font-weight-bold">31. januar</h3>
-        <div class="timeline">
-          <ProgramItem time-start="10:00">
-            <h3 class="font-weight-bold">Nettverksdagen åpner</h3>
-            <p class="description">Alle bedriftene står klare i Glassgården kl 10 for å slå av en prat.</p>
-          </ProgramItem>
-          <ProgramItem time-start="10:00" time-end="11:00" place="H1">
-            <h4 class="mb-1">Bedriftspresentasjon med SINTEF</h4>
+          <ProgramItem time-start="">
+            <h3 class="font-weight-bold">Nettverksdagene åpner</h3>
           </ProgramItem>
         </div>
       </div>
