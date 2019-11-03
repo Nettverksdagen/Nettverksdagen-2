bcd0ff56f647fe7c4316ddc5c1fbe6131479e222
Erlend Blomseth <erlend@elendig.no>
2019-11-03 20:00:19 +0100

    Remove ghost from autodeploy

--- autodeploy.sh
+++ autodeploy.sh
@@ -10,4 +10,4 @@ docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml stop nginx
 yes | docker-compose -f docker-compose-prod.yml rm nginx
 docker volume rm nettverksdagen-2_frontend-files
-docker-compose -f docker-compose-prod.yml up --no-deps -d nginx fileserver api frontend ghost postgres
+docker-compose -f docker-compose-prod.yml up --no-deps -d nginx fileserver api frontend postgres
