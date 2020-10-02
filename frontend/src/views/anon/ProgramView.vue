7a8b4b51d60e46ba1df79b433d9dc2ca6f5278a7
Kristoffer Skare <kristoffer.skare@live.com>
2020-10-02 16:35:53 +0200

    Added dialogBox and fixed formating

--- frontend/src/views/anon/ProgramView.vue
+++ frontend/src/views/anon/ProgramView.vue
@@ -4,22 +4,23 @@
         <h1 class="text-center">Program</h1>
         <p class="text-center description mt-3 mb-2">Dette var programmet for Nettverksdagene 2020.</p>
         <h4 class="text-center font-weight-bold"><a href="#stand-map-header">Se standkart her!</a></h4>
-        <div v-for="(day, index) in program">
+        <div :key="'programDay' + index" v-for=" (day , index) in program">
           <h3 class="font-weight-bold">{{formatDate(day[0].timeStart)}}</h3>
           <div class="timeline">
-            <div v-for="(item, index) in day" >
-              <ProgramItem 
-              :timeStart="item.timeStart" 
-              :timeEnd="item.timeEnd" 
-              :place="item.place" 
-              :header="item.header" 
-              :paragraph="item.paragraph" 
-              :registration="item.registration" 
+            <div :key="'dayItem' + index" v-for="(item, index) in day">
+              <ProgramItem
+              :timeStart="item.timeStart"
+              :timeEnd="item.timeEnd"
+              :place="item.place"
+              :header="item.header"
+              :paragraph="item.paragraph"
+              :registration="item.registration"
               :maxRegistered="item.maxRegistered"
               :registered="item.registered"
               :cancelEmail="item.cancelEmail"
               :registrationStart="item.registrationStart"
               :registrationEnd="item.registrationEnd"
+              :name="item.key"
               >
               </ProgramItem>
             </div>
@@ -44,200 +45,194 @@ export default {
     ProgramItem
   },
   methods: {
-    formatDate(dateObj){
-      let months = ["januar"]
-      let date = dateObj.getDate();
-      let month = dateObj.getMonth();
-      return String(date) + ". " + months[month]
+    formatDate (dateObj) {
+      let months = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember']
+      let date = dateObj.getDate()
+      let month = dateObj.getMonth()
+      return String(date) + '. ' + months[month]
     }
   },
   computed: {
-    program: function(){
+    program: function () {
       let prog = [
         {
           key: 3,
-          timeStart: new Date(2021, 0, 29, 10, 0, 0,0),
-          place:'Glassgården, Elektrobygget', 
-          header: 'Standområdet åpner!',
+          timeStart: new Date(2021, 0, 29, 10, 0, 0, 0),
+          place: 'Glassgården, Elektrobygget',
+          header: 'Standområdet åpner!'
         },
-        
         {
           key: 4,
-          timeStart: new Date(2021, 0, 29, 12, 10, 0,0),
-          place:'Glassgården, Elektrobygget', 
-          header: 'Ballongslipp!',
+          timeStart: new Date(2021, 0, 29, 12, 10, 0, 0),
+          place: 'Glassgården, Elektrobygget',
+          header: 'Ballongslipp!'
         },
         {
           key: 8,
-          timeStart: new Date(2021, 0, 29, 17, 15, 0,0), 
-          place:'EL1, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 29, 17, 15, 0, 0),
+          place: 'EL1, Gamle Elektro',
           header: 'Bedriftpresentasjon med Bekk',
-          paragraph: ['Med bespisning på Olivia Solsiden', 'Dette arrangementet er fullt.'],
+          paragraph: ['Med bespisning på Olivia Solsiden', 'Dette arrangementet er fullt.']
         },
-         {
+        {
           key: 1,
-          timeStart: new Date(2021, 0, 28, 17, 30, 0,0),
-          place:'EL1, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 28, 17, 30, 0, 0),
+          timeEnd: new Date(2021, 0, 28, 18, 30, 0, 0),
+          place: 'EL1, Gamle Elektro',
           header: 'Bedriftpresentasjon med Honeywell',
           paragraph: ['Med bespisning på Graffi Grill.', 'Dette arrangementet er fullt.'],
           registration: true,
           maxRegistered: 10,
           registered: 5,
-          cancelEmail: "kska@stud.ntnu.no",
-          registrationStart: new Date(2020, 8, 16, 18, 58, 0,0),
+          cancelEmail: 'kska@stud.ntnu.no',
+          registrationStart: new Date(2020, 8, 18, 10, 33, 0, 0)
         },
-      
-       {
+        {
           key: 6,
-          timeStart: new Date(2021, 0, 29, 14, 0, 0,0),
-          place:'Glassgården, Elektrobygget', 
-          header: 'Premieutdeling!',
+          timeStart: new Date(2021, 0, 29, 14, 0, 0, 0),
+          place: 'Glassgården, Elektrobygget',
+          header: 'Premieutdeling!'
         },
         {
           key: 7,
-          timeStart: new Date(2021, 0, 29, 14, 15, 0,0),
-          timeEnd: new Date(2021, 0, 29, 15, 0, 0,0), 
-          place:'EL1, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 29, 14, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 29, 15, 0, 0, 0),
+          place: 'EL1, Gamle Elektro',
           header: 'Bedriftpresentasjon med Imatis',
           paragraph: ['Med servering av baguetter og mineralvann.', 'Påmeldingen er avsluttet.']
         },
-        
-          {
+        {
           key: 5,
-          timeStart: new Date(2021, 0, 29, 12, 15, 0,0),
-          timeEnd: new Date(2021, 0, 29, 13, 0, 0,0), 
-          place:'KJL1, Kjelhuset', 
+          timeStart: new Date(2021, 0, 29, 12, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 29, 13, 0, 0, 0),
+          place: 'KJL1, Kjelhuset',
           header: 'Prosjektpresentasjon med Data Respons R&D Services',
           paragraph: ['Bli med på Kahootkonkurranse og vinn AirPods Pro.', 'Påmeldingen er avsluttet.']
         },
         {
           key: 9,
-          timeStart: new Date(2021, 0, 30, 9, 15, 0,0),
-          timeEnd: new Date(2021, 0, 30, 10, 15, 0,0), 
-          place:'EL5, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 30, 9, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 30, 10, 15, 0, 0),
+          place: 'EL5, Gamle Elektro',
           header: 'Frokostpresentasjon med National Oilwell Varco',
           paragraph: ['Med servering av baguetter og kaffe.', 'Påmeldingen er avsluttet.']
         },
-        
         {
           key: 2,
-          timeStart: new Date(2021, 0, 29, 9, 15, 0,0),
-          timeEnd: new Date(2021, 0, 29, 10, 15, 0,0), 
-          place:'EL6, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 29, 9, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 29, 10, 15, 0, 0),
+          place: 'EL6, Gamle Elektro',
           header: 'Frokostpresentasjon med Sopra Steria',
           paragraph: ['Med servering av baguetter og kaffe.', 'Påmeldingen er avsluttet.']
         },
         {
           key: 11,
-          timeStart: new Date(2021, 0, 30, 11, 15, 0,0),
-          timeEnd: new Date(2021, 0, 30, 12, 0, 0,0), 
-          place:'S8, Stripa (Sentralbygg 2)', 
+          timeStart: new Date(2021, 0, 30, 11, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 30, 12, 0, 0, 0),
+          place: 'S8, Stripa (Sentralbygg 2)',
           header: 'Lønsjpresentasjon med Maritime Robotics',
           paragraph: ['Med servering av baguetter og mineralvann.', 'Påmeldingen er avsluttet.']
         },
         {
           key: 12,
-          timeStart: new Date(2021, 0, 30, 12, 10, 0,0),
-          place:'Glassgården, Elektrobygget', 
-          header: 'Ballongslipp!',
+          timeStart: new Date(2021, 0, 30, 12, 10, 0, 0),
+          place: 'Glassgården, Elektrobygget',
+          header: 'Ballongslipp!'
         },
         {
           key: 13,
-          timeStart: new Date(2021, 0, 30, 12, 15, 0,0),
-          timeEnd: new Date(2021, 0, 30, 13, 0, 0,0), 
-          place:'S1, Stripa (Sentralbygg 1)', 
+          timeStart: new Date(2021, 0, 30, 12, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 30, 13, 0, 0, 0),
+          place: 'S1, Stripa (Sentralbygg 1)',
           header: 'Prosjektpresentasjon med Data Respons R&D Services',
           paragraph: ['Bli med på Kahootkonkurranse og vinn AirPods Pro.', 'Påmeldingen er avsluttet.']
         },
         {
           key: 10,
-          timeStart: new Date(2021, 0, 30, 10, 0, 0,0),
-          place:'Glassgården, Elektrobygget', 
+          timeStart: new Date(2021, 0, 30, 10, 0, 0, 0),
+          place: 'Glassgården, Elektrobygget',
           header: 'Standområdet åpner',
           paragraph: ['Goodiebags til de 50 første som kommer innom!']
         },
         {
           key: 14,
-          timeStart: new Date(2021, 0, 30, 14, 0, 0,0),
-          place:'Glassgården, Elektrobygget', 
-          header: 'Premieutdeling!',
+          timeStart: new Date(2021, 0, 30, 14, 0, 0, 0),
+          place: 'Glassgården, Elektrobygget',
+          header: 'Premieutdeling!'
         },
-         {
+        {
           key: 15,
-          timeStart: new Date(2021, 0, 30, 14, 15, 0,0),
-          timeEnd: new Date(2021, 0, 30, 15, 30, 0,0), 
-          place:'EL6, Gamle Elektro', 
+          timeStart: new Date(2021, 0, 30, 14, 15, 0, 0),
+          timeEnd: new Date(2021, 0, 30, 15, 30, 0, 0),
+          place: 'EL6, Gamle Elektro',
           header: 'Kryptografi-skattejakt med DNB',
           paragraph: ['Med servering av baguetter og mineralvann. Premie til vinnerne!', 'Påmeldingen er avsluttet.']
-        },        
-      ];
-      let sortedProgram = [];
-      let days = [];
+        }
+      ]
+
+      let sortedProgram = []
+      let days = []
       // It now should sort, but maybe uneficiently
       prog.forEach((item, index) => {
-        if(index===0){
-          sortedProgram.push([item]);
-          days.push(item.timeStart.getDate());
-        }else{
-          let newSortedProgram = sortedProgram;
-          let dayExists = false;
-          let itemInserted = false;
-            days.forEach((day, index) => {
-              if(item.timeStart.getDate() == day){
-                dayExists = true
-                let newDayItems = [];
-                for(let j=0; j<newSortedProgram[index].length; j++){
-                  if(newSortedProgram[index][j].timeStart.getTime()>item.timeStart.getTime() && !itemInserted){
-                    itemInserted=true;
-                    newDayItems.push(item);
-                  }
-                    newDayItems.push(newSortedProgram[index][j])
-                  
+        if (index === 0) {
+          sortedProgram.push([item])
+          days.push(item.timeStart.getDate())
+        } else {
+          let newSortedProgram = sortedProgram
+          let dayExists = false
+          let itemInserted = false
+          days.forEach((day, index) => {
+            if (item.timeStart.getDate() === day) {
+              dayExists = true
+              let newDayItems = []
+              for (let j = 0; j < newSortedProgram[index].length; j++) {
+                if (newSortedProgram[index][j].timeStart.getTime() > item.timeStart.getTime() && !itemInserted) {
+                  itemInserted = true
+                  newDayItems.push(item)
                 }
-                if(!itemInserted){
-                  newDayItems.push(item);
-                }
-                newSortedProgram[index] = newDayItems;
+                newDayItems.push(newSortedProgram[index][j])
               }
-            })
-          if(!dayExists){
-            let newDate = item.timeStart.getDate();
-            let inserted = false;
-            let nextDays = [];
-            let nextNewSortedProg = [];
-            for( let i = 0; i<days.length; i++){
-              if(days[i]>newDate && !inserted){
-                inserted=true;
-                nextDays.push(newDate);
-                nextNewSortedProg.push([item]);
+              if (!itemInserted) {
+                newDayItems.push(item)
               }
-                nextDays.push(days[i]);
-                nextNewSortedProg.push(newSortedProgram[i]);
-              
+              newSortedProgram[index] = newDayItems
             }
-            newSortedProgram = nextNewSortedProg;
-            days = nextDays;
-            if(!inserted){
-              days.push(newDate);
-              newSortedProgram.push([item]);
+          })
+          if (!dayExists) {
+            let newDate = item.timeStart.getDate()
+            let inserted = false
+            let nextDays = []
+            let nextNewSortedProg = []
+            for (let i = 0; i < days.length; i++) {
+              if (days[i] > newDate && !inserted) {
+                inserted = true
+                nextDays.push(newDate)
+                nextNewSortedProg.push([item])
+              }
+              nextDays.push(days[i])
+              nextNewSortedProg.push(newSortedProgram[i])
+            }
+            newSortedProgram = nextNewSortedProg
+            days = nextDays
+            if (!inserted) {
+              days.push(newDate)
+              newSortedProgram.push([item])
             }
           }
-          sortedProgram = newSortedProgram;
+          sortedProgram = newSortedProgram
         }
       })
-
       /*
       // Testing if it is sorted
       sortedProgram.forEach((day) => {
         day.forEach((item) => {
-          console.log(item.key);
+          console.log(item.key)
         })
       })
       */
       return sortedProgram
-}
+    }
   }
-  
 }
 </script>
 
