3ba5f338a5312a503c7787dc1de9fee2f99f57f4
Henrik Jordheim <henjord@hotmail.com>
2020-10-16 10:41:08 +0200

    Added fnuts to the default email settings

--- api/nvdnew/settings/mail_settings.py
+++ api/nvdnew/settings/mail_settings.py
@@ -1,10 +1,10 @@
 ####### EMAIL STUFF #########
 
-EMAIL_HOST =            #'mail.uniweb.no'
-EMAIL_HOST_USER =       #'it@nettverksdagene.no'
-EMAIL_HOST_PASSWORD =   #'Your Password
-EMAIL_PORT =            #587
-EMAIL_USE_TLS =         #True
-EMAIL_USE_SSL =         #False
+EMAIL_HOST = ''           #'mail.uniweb.no'
+EMAIL_HOST_USER = ''      #'it@nettverksdagene.no'
+EMAIL_HOST_PASSWORD = ''  #'Your Password
+EMAIL_PORT = ''           #587
+EMAIL_USE_TLS = ''        #True
+EMAIL_USE_SSL = ''        #False
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
