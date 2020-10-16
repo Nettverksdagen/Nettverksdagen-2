d0981e1f2f7776eb1593c4a627bced1e241be4fd
Henrik Jordheim <henjord@hotmail.com>
2020-10-16 11:01:29 +0200

    created a send_email function

--- api/nvdagen/views.py
+++ api/nvdagen/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+
+
+#
+def send_email(email, relevant_arrangement):
+    try:
+        #authentication = authentication_email()
+        send_mail('Nettverksdagene - Påmelding til ' + relevant_arrangement,
+        'Vennligst verifiser din påmelding ved å klikke på denne linken: ' + authentication,
+        'it@nettverksdagene.no',
+        [email],
+        fail_silently=False)
+    except:
+        print("MÅ STÅ NOE VETTUGT HER")
+
+    # return render(request, '')
+
+def authentication_email():
+    #Henter random streng som er generert i frontend
+    #returnerer den genererte strengen
