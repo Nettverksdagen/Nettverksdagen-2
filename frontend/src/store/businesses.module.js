925391168c731be78f500f10ef4a4928d8dc0d2a
Jakob Tiller <jakobtiller@gmail.com>
2021-09-28 20:59:11 +0200

    changed 2021 to 2022 on home page

--- frontend/src/store/businesses.module.js
+++ frontend/src/store/businesses.module.js
@@ -21,8 +21,8 @@ const actions = {
 const getters = {
   levels: state => {
     let levels = [
-      {level: 'Hovedsamarbeidspartner', levelHeader: 'Hoved&shy;samarbeids&shy;partner 2021', businesses: []},
-      {level: 'Samarbeidspartner', levelHeader: 'Samarbeids&shy;partner 2021', businesses: []},
+      {level: 'Hovedsamarbeidspartner', levelHeader: 'Hoved&shy;samarbeids&shy;partner 2022', businesses: []},
+      {level: 'Samarbeidspartner', levelHeader: 'Samarbeids&shy;partner 2022', businesses: []},
       {level: 'Gull', levelHeader: 'Bedrifter du kan møte på stand', businesses: []},
       {level: 'Sølv', levelHeader: '', businesses: []},
       {level: 'Bronse', levelHeader: '', businesses: []}
