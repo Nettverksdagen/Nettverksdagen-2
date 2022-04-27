d964efe3b75df6dc178073914f0d91eb24966eb7
Håkon Bårsaune <haakbaa@stud.ntnu.no>
2022-04-27 17:00:12 +0200

    add default mail configuration file

---
+++ api/nvdnew/settings/mail_settings_default.py
@@ -0,0 +1,13 @@
+EMAIL_HOST = 'mail.uniweb.no'
+
+EMAIL_HOST_USER = 'do-not-reply@nettverksdagene.no'
+
+EMAIL_HOST_PASSWORD = ''
+
+EMAIL_PORT = '587'
+
+EMAIL_USE_TLS = True
+
+EMAIL_USE_SSL = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
