527d4e4b6ac5d5a2a8cd0dc178a9259830ef616c
Saim Iqbal <saim.iqbal@shifthyperloop.com>
2022-09-28 15:37:29 +0200

    Added outline

--- frontend/src/views/anon/HomeView.vue
+++ frontend/src/views/anon/HomeView.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="home">
-    <div class="text-center pt-5 card">
-      <h3>Vil din bedrift delta på Nettverksdagene 2023?</h3>
-      <h3><a href="mailto:bedrift@nettverksdagene.no"> Send mail til bedrift@nettverksdagene.no </a></h3>
-    </div>
     <div class="dude">
       <!--<Hero/> -->
       <HomeScreen/>
