fd13235a88f3eb9cbc113895a38dcd24a837fef6
Saim Iqbal <saim.iqbal@shifthyperloop.com>
2022-09-28 15:27:44 +0200

    Fix type

--- frontend/src/store/businesses.module.js
+++ frontend/src/store/businesses.module.js
@@ -21,7 +21,7 @@ const actions = {
 const getters = {
   levels: state => {
     let levels = [
-      {level: 'Hovedsamarbeidspartner', levelHeader: 'Møt vår nyte hovedsamarbeidspartner ', businesses: []},
+      {level: 'Hovedsamarbeidspartner', levelHeader: 'Møt vår nye hovedsamarbeidspartner ', businesses: []},
       {level: 'Samarbeidspartner', levelHeader: 'Samarbeids&shy;partnere 2022', businesses: []},
       {level: 'Gull', levelHeader: 'Bedrifter du kunne møte på stand i 2022', businesses: []},
       {level: 'Sølv', levelHeader: '', businesses: []},
--- frontend/src/views/anon/HomeView.vue
+++ frontend/src/views/anon/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="text-center pt-5">
+    <div class="text-center pt-5 card">
       <h3>Vil din bedrift delta på Nettverksdagene 2023?</h3>
       <h3><a href="mailto:bedrift@nettverksdagene.no"> Send mail til bedrift@nettverksdagene.no </a></h3>
     </div>
@@ -37,5 +37,12 @@ export default {
 <style lang="scss" scoped>
   .dude {
     width:100%;
+  }
+  .card {
+    position: relative;
+    border-radius: 20px;
+    border-width: 2px;
+    border-color: var(--line-border-color);
+    background-color: white;
   }
 </style>
