2a6a2bff21386497b11a6f3ec6ad3c73d696bf7e
Eivind Møen <eivind0598@gmail.com>
2023-01-19 16:02:09 +0100

    Lykkehjul update

---
+++ frontend/static/lykkehjul/js/initWindow.js
@@ -0,0 +1,10 @@
+function applySettings()
+{
+    document.getElementById("initWindow").style.display = "none";
+    readFile();
+}
+
+function updateRaduis()
+{
+    wheelsize = document.getElementById("radiusInp").value;
+}
