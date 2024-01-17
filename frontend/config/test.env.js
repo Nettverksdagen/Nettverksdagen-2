462b66463e8498be4266093f92851bc876018e93
Andreas Traa Utkilen <andretut@stud.ntnu.no>
2024-01-17 22:53:06 +0100

    hide standmap until fixed

--- frontend/config/test.env.js
+++ frontend/config/test.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"testing"',
   VUE_APP_API_HOST: '"http://34.88.95.69/"',
   VUE_APP_FILESERVER_HOST: '"http://34.88.95.69/"'
 })
