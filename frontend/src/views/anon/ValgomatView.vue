b335472ebf9a63d165e841b8f597c587cbcf00bb
Mona Victoria Vahedi <mokamv3@gmail.com>
2024-10-25 12:32:10 +0200

    fix: script for quiz system

--- frontend/src/views/anon/ValgomatView.vue
+++ frontend/src/views/anon/ValgomatView.vue
@@ -4,15 +4,17 @@
         <h1>{{$t('about') + ' ' + $t('nettverksdagene')}}</h1>
         <b-row>
           <b-col cols="12" lg="6">
-            <p class="introduction">
-              {{$t('nettis')}}
-              <br>
-              <br>
-              {{$t('inntektene')}}
-            </p>
-          </b-col>
-          <b-col cols="12" lg="6" class="d-block d-sm-block">
-            <b-img class="image" src="@/assets/excited_people.jpg" fluid alt="Engasjerte studenter i Glassgården"/>
+            <div>
+              <!-- Render the quiz questions and results here -->
+              <QuestionCard
+                v-for="question in questions"
+                :key="question.id"
+                :question="question"
+                @answerSelected="handleAnswer"
+              />
+              <Result v-if="showResults" :topCompanies="topCompanies" />
+              <ProgressBar :progress="progress" />
+            </div>
           </b-col>
         </b-row>
       </Content>
@@ -21,12 +23,78 @@
   
   <script>
   import Content from '@/components/common/Content.vue'
+  import QuestionCard from '@/components/valgomat/QuestionCard.vue'
+  import Result from '@/components/valgomat/Result.vue'
+  import ProgressBar from '@/components/valgomat/ProgressBar.vue'
+
   export default {
-    name: 'ValgomatView',
     components: {
-      Content
-    }
-  }
+      Content,
+      QuestionCard,
+      Result,
+      ProgressBar
+    },
+    data() {
+      return {
+        showResults: false,
+        progress: 0,
+        topCompanies: [], // Stores top 3 companies based on the user's answers
+
+        // Questions and answers for the quiz
+        questions: [
+          {
+            id: 1,
+            text: "What type of work environment do you prefer?",
+            options: [
+              { id: 1, text: "Collaborative", companyWeights: { CompanyA: 1, CompanyB: 2, CompanyC: 3 } },
+              { id: 2, text: "Independent", companyWeights: { CompanyA: 3, CompanyB: 2, CompanyC: 1 } },
+            ],
+          },
+          {
+            id: 2,
+            text: "What is your preferred programming language?",
+            options: [
+              { id: 1, text: "JavaScript", companyWeights: { CompanyA: 1, CompanyB: 2, CompanyC: 3 } },
+              { id: 2, text: "Python", companyWeights: { CompanyA: 3, CompanyB: 2, CompanyC: 1 } },
+            ],
+          },
+          {
+            id: 3,
+            text: "What is your preferred development methodology?",
+            options: [
+              { id: 1, text: "Agile", companyWeights: { CompanyA: 1, CompanyB: 2, CompanyC: 3 } },
+              { id: 2, text: "Waterfall", companyWeights: { CompanyA: 3, CompanyB: 2, CompanyC: 1 } },
+            ],
+          },
+        ],
+        selectedAnswers: {}, // Stores the user's selected answers by question ID
+      };
+    },
+    methods: {
+      handleAnswer(questionId, option) {
+        this.selectedAnswers[questionId] = option;
+        this.calculateProgress();
+        this.updateProgress();
+      },
+      calculateScores() {
+        const companyScores = {};
+        for (const answer of Object.values(this.selectedAnswers)) {
+          for (const [company, score] of Object.entries(answer.companyWeights)) {
+            companyScores[company] = (companyScores[company] || 0) + score;
+          }
+        }
+        //Sort and store the top 3 companies based on the scores
+        this.topCompanies = Object.entries(companyScores)
+          .sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
+          .slice(0, 3)
+          .map(([company]) => company);
+      },
+      updateProgress() {
+        this.progress = (Object.keys(this.selectedAnswers).length / this.questions.length) * 100;
+        if (this.progress === 100) this.showResults = true;
+      },
+    },
+  };
   </script>
   
   <style lang="scss" scoped>
