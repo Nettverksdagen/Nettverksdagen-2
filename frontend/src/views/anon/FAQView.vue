3f462aca071c47367ecdcb80e6adc917cc0a5883
Theodor Johansson <theodor.lund.johansson@gmail.com>
2025-01-19 22:54:13 +0100

    refactor: ensured FAQ stays consistent with all other pages. Removed unnecessary style definitions.

--- frontend/src/views/anon/FAQView.vue
+++ frontend/src/views/anon/FAQView.vue
@@ -1,74 +1,38 @@
 <template>
   <div id="appFAQ">
-    <div class="content-wrapper">
-      <div class="faq-container">
-        <h1 class="page-header">{{$t('faq.header')}}</h1>
-        <FaqList :faqs="$t('faq.questions')" />
-      </div>
-      <img src="@/assets/NVDBilde2.jpg" alt="Related image" class="side-image" />
-    </div>
+    <Content>
+      <h1 class="page-header">{{$t('faq.header')}}</h1>
+      <b-row>
+        <b-col cols="12" lg="6">
+          <FaqList :faqs="$t('faq.questions')" />
+        </b-col>
+        <b-col cols="12" lg="6" class="d-block d-sm-block">
+          <b-img src="@/assets/NVDBilde2.jpg" fluid alt="Related image" class="side-image"/>
+        </b-col>
+      </b-row>
+    </Content>
   </div>
 </template>
 
 <script>
+import Content from '@/components/common/Content.vue'
 import FaqList from '@/components/anon/FaqList.vue'
 
 export default {
   name: 'FAQView',
   components: {
+    Content,
     FaqList
   }
 }
 </script>
 
-<style scoped>
-#appFAQ {
-  font-family: Arial, sans-serif;
-  padding: 0; /* Remove default padding */
-}
-
-.content-wrapper {
-  display: flex;
-  align-items: flex-start;
-  width: 100%; /* Full screen width */
-  /* padding: 20px; */
-  flex-direction: row; /* Default: items side by side */
-}
-
-.faq-container {
-  flex: 3;
-  margin-right: 20px; /* Space between FAQ and image */
-}
-
-/* Padding below header */
-.faq-container h1 {
-  margin-bottom: 30px; /* Adjust this value for more or less spacing */
-}
-
-.side-image {
-  flex: 1;
-  max-width: 600px;
-  height: auto;
-  border-radius: 10px;
-  object-fit: cover;
-  margin-right: 20px;
-  margin-top: 80px;
-}
-
-/* Media query for smaller screens */
-@media (max-width: 80%) { /* Adjust the width as needed for phones */
-  .content-wrapper {
-    flex-direction: column; /* Stack items vertically */
-    align-items: center; /* Center content horizontally */
-  }
-
-  .faq-container {
-    margin-right: 0; /* Remove right margin for vertical stacking */
+<style scoped lang="scss">
+  #appFAQ {
+    flex: 1;
   }
 
   .side-image {
-    margin-top: 20px; /* Add spacing between content and image */
-    margin-right: 0; /* Remove right margin for alignment */
+    border-radius: 20px;
   }
-}
 </style>
