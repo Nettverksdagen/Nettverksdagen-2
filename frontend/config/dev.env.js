a74e54ee1d9aa15e27142579e04e1b7ac7cab8e3
Afras <afras@hotmail.no>
2025-10-07 12:36:49 +0200

    chore: update API host to use localhost

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_API_HOST: '"http://127.0.0.1:8000"',
+  VUE_APP_API_HOST: '"http://localhost:8000"',
   VUE_APP_FILESERVER_HOST: '"http://127.0.0.1:9000"'
 })
