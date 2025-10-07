cdc148eb4d9ac335720a646a6ac1d101b0814a91
Afras <afras@hotmail.no>
2025-10-07 12:24:24 +0200

    feat: add ParticipantAttendanceSerializer

--- api/nvdagen/serializers.py
+++ api/nvdagen/serializers.py
@@ -45,3 +45,8 @@ class ParticipantListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         exclude = ('code',)
+
+class ParticipantAttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ('id', 'name', 'email', 'study', 'year', 'attendance_token', 'attended', 'check_in_time', 'event')
