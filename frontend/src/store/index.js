1d33269730895cb8573520307f454222a3f4e35e
Edvard Dingsør <edvard.wdingsor@gmail.com>
2026-01-21 17:30:37 +0100

    feat: fix frontend for FAQ admin view and refactor FAQ to read from db

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,7 +8,12 @@ import { teamMembers } from './teamMembers.module'
 import { forms } from './forms.module'
 import { program } from './program.module'
 import { participant } from './participant.module'
+<<<<<<< HEAD
 import { infobox } from './infobox.module'
+||||||| parent of 624049c (feat: fix frontend for FAQ admin view and refactor FAQ to read from db)
+=======
+import { faq } from './faq.module'
+>>>>>>> 624049c (feat: fix frontend for FAQ admin view and refactor FAQ to read from db)
 import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
@@ -21,8 +26,15 @@ export default new Vuex.Store({
     teamMembers,
     forms,
     program,
+<<<<<<< HEAD
     participant,
     infobox
+||||||| parent of 624049c (feat: fix frontend for FAQ admin view and refactor FAQ to read from db)
+    participant
+=======
+    participant,
+    faq
+>>>>>>> 624049c (feat: fix frontend for FAQ admin view and refactor FAQ to read from db)
   },
   plugins: [createPersistedState()]
 })
