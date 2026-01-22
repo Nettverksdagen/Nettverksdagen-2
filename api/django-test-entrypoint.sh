a9302258017209af91b4c2839ee72573114dcc88
Edvard Dingsør <edvard.wdingsor@gmail.com>
2026-01-22 01:13:24 +0100

    fix: change test-entrypoint to load data from template

--- api/django-test-entrypoint.sh
+++ api/django-test-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 sh django-base-entrypoint.sh
 
 echo "Loading data from fixtures"
-python manage.py loaddata fixtures.json
+python manage.py loaddata nvdagen/fixtures/fixtures.template.json
 
 echo "Running tests"
 python manage.py test test
